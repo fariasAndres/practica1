411c840b7fd07d7517d07283f1f3e79264922b8b
Andres <Andres@Andres-PC.lan>
2013-09-14 18:08:27 -0500

    chave

--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!--   chave chave-->
         <title></title>
     </head>
     <body>
         
         
         <?php
+        
         gdthdturtu
         // put your code here
         ?>
